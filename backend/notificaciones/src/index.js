6b3b9fda6fe295e095a5d1403bccd2e5f146d9cd
Brian Aguinsaca <brian.aguinsaca@unl.edu.ec>
2025-07-22 11:52:51 -0500

    Creación del panel base de notificaciones

--- backend/notificaciones/src/index.js
+++ backend/notificaciones/src/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Importar rutas de notificaciones
+const notificacionesRoutes = require('./routes/notificacionesRoutes');
 
 app.get('/', (req, res) => {
   res.send('Microservicio de notificaciones funcionando');
 });
 
+// Rutas para notificaciones
+app.use('/api/notificaciones', notificacionesRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Users service listening on port ${PORT}`);
+  console.log(`Notificaciones service listening on port ${PORT}`);
 });
